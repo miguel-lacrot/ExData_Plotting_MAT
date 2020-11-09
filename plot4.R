ce9b3fa77f3c3bc495b9ae90e0dd9c98bf989a5b
Miguel Lacrot <mikeator@gmail.com>
2020-11-09 18:00:22 +0100

    Correct missing code on plot4.R for generating the 4th graph

--- plot4.R
+++ plot4.R
@@ -23,6 +23,8 @@ with(powerSub, lines(Time, Sub_metering_3, col= "blue"))
 legend("topright", bty ="n", cex = 0.7, lty = c(1,1,1), 
        col = c("black", "red", "blue"), 
        legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+plot(powerSub$Time, powerSub$Global_reactive_power, type = "l", 
+     ylab = "Global reactive power", xlab = "")
 ##Save as png
 dev.copy(png, file = "plot4.png")
 dev.off()
